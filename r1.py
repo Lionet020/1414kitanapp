58b2c206347ae3e918f7643132890b96aa8cb161
Lionet <136951662+Lionet020@users.noreply.github.com>
2023-06-19 13:33:37 +0900

    Update r1.py

--- r1.py
+++ r1.py
@@ -1,18 +1,25 @@
+import tkinter as tk
+from tkinter import ttk, filedialog, messagebox
+import os
+from PIL import Image, ImageTk
+import json
+
 def launch_tool(tool_path):
-    current_dir = os.getcwd()  # 現在のカレントディレクトリを保存
-
-    tool_dir = os.path.dirname(tool_path)  # ツールのディレクトリパスを取得
-    os.chdir(tool_dir)  # カレントディレクトリをツールのディレクトリに変更
-
     file_extension = os.path.splitext(tool_path)[1].lower()
-    if file_extension in [".py", ".pyw"]:
-        os.system(f'pythonw {tool_path}')  # Pythonスクリプトを実行
-    elif file_extension == ".pl":
-        os.system(f'perl {tool_path}')  # Perlスクリプトを実行
-    elif file_extension in [".xlsx", ".xlsm", ".txt"]:
-        os.startfile(tool_path)  # エクセルファイルやテキストファイルを開く
-    else:
-        messagebox.showwarning("エラー", "サポートされていないファイル形式です。")
+    tool_directory = os.path.dirname(tool_path)
+    current_directory = os.getcwd()
 
-    os.chdir(current_dir)  # カレントディレクトリを元に戻す
+    try:
+        os.chdir(tool_directory)  # Change the current working directory
+        if file_extension == ".py" or file_extension == ".pyw":
+            os.system(f'python "{tool_path}"')  # Execute Python file
+        elif file_extension == ".pl":
+            os.system(f'perl "{tool_path}"')  # Execute Perl file
+        elif file_extension in [".xlsx", ".xlsm", ".txt"]:
+            os.startfile(tool_path)  # Open Excel or text file
+        else:
+            messagebox.showwarning("エラー", "サポートされていないファイル形式です。")
+    finally:
+        os.chdir(current_directory)  # Restore the original current working directory
 
+# Rest of the code remains the same...
