3ac7266a3b83e78a839c41639897c4f03fe38e4a
Lionet <136951662+Lionet020@users.noreply.github.com>
2023-08-23 18:33:55 +0900

    Create BubunToTaisyoulog.py

---
+++ BubunToTaisyoulog.py
@@ -0,0 +1,27 @@
+import os
+
+# フォルダ内のファイルを取得
+files = os.listdir()
+target_files = [filename for filename in files if filename[:2].isdigit() and filename.endswith("_aaa.txt")]
+
+# 検索ワード
+search_word = "======================================================"
+
+# ファイルごとに処理
+for filename in target_files:
+    with open(filename, "r") as file:
+        content = file.read()
+        occurrences = content.count(search_word)
+        
+        # 出力ファイル名
+        output_filename = filename[:2] + "_bbb.txt"
+        
+        # 出力内容
+        if occurrences < 4:
+            output_content = content
+        else:
+            output_content = "\n".join(content.split(search_word)[:4])
+        
+        # 出力ファイルに書き込み
+        with open(output_filename, "w") as output_file:
+            output_file.write(output_content)
