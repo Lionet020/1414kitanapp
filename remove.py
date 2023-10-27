bf11e02ea0810c389655aa333768291333fa2d4a
Lionet <136951662+Lionet020@users.noreply.github.com>
2023-10-27 14:51:54 +0900

    Update remove.py

--- remove.py
+++ remove.py
@@ -9,19 +9,20 @@ def merge_unique_data(file_path):
     for line in lines:
         if line.startswith('********* http'):
             if current_key is not None:
-                merged_data[current_key] = current_data
+                merged_data.setdefault(current_key, []).extend(current_data)
             current_key = line.strip()
-            current_data = []
+            current_data = [line.strip()]
         else:
             current_data.append(line.strip())
 
     if current_key is not None:
-        merged_data[current_key] = current_data
+        merged_data.setdefault(current_key, []).extend(current_data)
 
     with open('output.txt', 'w', encoding='shift-jis') as output_file:
         for key, data in merged_data.items():
+            unique_data = list(set(data))  # データ部分をユニークにする
             output_file.write(key + '\n')
-            output_file.write('\n'.join(data) + '\n\n')
+            output_file.write('\n'.join(unique_data) + '\n\n')
 
 # 使い方の例
 merge_unique_data('your_text_file.txt')
